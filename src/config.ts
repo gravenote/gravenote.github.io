c17c1f8c8c1fb166d00013b7d9536b63c9243e5b
Notes of GraveNation <samael@mailfence.com>
2025-12-03 18:50:30 +0630

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://gravenote.github.io/",
+  author: "Grave Note",
+  profile: "https://www.thesmos.asia/",
+  desc: "NoteBook of Grave-Nation.",
+  title: "Grave-Note",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
